528b52cda03a3fa33db981fd106c2d5877b92d7b
harith m <harithmech@gmail.com>
2020-08-31 19:23:11 +0530

    added node to linked list

--- linked.py
+++ linked.py
@@ -7,5 +7,16 @@ class Linkedlist:
     def __init__(self):
         self.head = None
 
+    def insertend(self, ele):
+        if self.head is None:
+            self.head = ele
+        else:
+            last_node = self.head
+            while True:
+                if last_node is None:
+                    break
+                else:
+                    last_node = last_node.next
+            last_node.next = ele
 first_ele = Node("harith")
 linkedlist = Linkedlist()
